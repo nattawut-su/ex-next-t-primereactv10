00a0e3cbc4871529ba744913e887a30df4733f42
nattawut.su <nattzx245@gmail.com>
2025-08-29 22:05:25 +0700

    create page chapter5

---
+++ src/app/(basic)/classic/chapter5/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import PersonModel from '@/models/person';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+function Chapter5() {
+  const [value, setValue] = useState<PersonModel>({ fname: '', lname: '' });
+  const router = useRouter();
+
+  useEffect(() => {
+    const data = localStorage.getItem('personData');
+    if (data) {
+      localStorage.removeItem('personData');
+    }
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    localStorage.setItem('personData', JSON.stringify(value));
+    router.push(`/classic/chapter5/result`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="fname">First Name:</label>
+        <input type="text" name="fname" value={value.fname} onChange={(e) => setValue((v) => ({ ...v, fname: e.target.value }))} />
+        <br />
+        <label htmlFor="lname">Last Name:</label>
+        <input type="text" name="lname" value={value.lname} onChange={(e) => setValue((v) => ({ ...v, lname: e.target.value }))} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Chapter5;
