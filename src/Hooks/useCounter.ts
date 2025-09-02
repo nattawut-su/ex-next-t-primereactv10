f0a76de53f77bddc947909763fd0479aa330def4
nattawut.su <nattzx245@gmail.com>
2025-09-02 11:01:00 +0700

    update code.

--- src/Hooks/useCounter.ts
+++ src/Hooks/useCounter.ts
@@ -4,7 +4,7 @@ import { useState } from 'react';
 export function useCounter(initialValue: number = 0) {
   const [count, setCount] = useState(initialValue);
 
-  // arrow functions ⇣⇣⇣⇣⇣
+  // arrow functions ⇣⇣⇣⇣
   const increment = () => setCount((c) => c + 1);
   const decrement = () => setCount((c) => c - 1);
   const reset = () => setCount(initialValue);
